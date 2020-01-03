1f632c4ad2adc6da760a1a2d9d301beff497571a
Djalma <djalma.ant@gmail.com>
2020-01-02 22:31:11 -0300

    Created the Entities the Enums and the Program

---
+++ Orders/Entities/OrderItem.cs
@@ -0,0 +1,25 @@
+﻿namespace Orders.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product product { get; set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int quantity, double price, Product product)
+        {
+            Quantity = quantity;
+            Price = price;
+            this.product = product;
+        }
+
+        public double SubTotal()
+        {
+            return Price * Quantity;
+        }
+    }
+}
